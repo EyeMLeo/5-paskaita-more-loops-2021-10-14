fe5519a643110c14105a34247ebbea8cde4a5770
EyeMLeo <Leonid.Akuta@gmail.com>
2022-10-14 13:03:43 +0200

    nuo skaiciaus 1sk iki 21sk suma

--- js/main.js
+++ js/main.js
@@ -1,2 +1,125 @@
 'use strict'
 console.log('main.js');
+
+
+// 20. sudauginti skaiciu nuo kintamojo min iki kintamojo max
+
+// min 5. max 10
+
+// sudauginti ir aspausdinti skaiciu daugybos rezultata
+
+// let min=5;
+// let max=10;
+// let suma=0;
+
+// for(let i=min;i<=max;i++){
+// if(i===min){
+// suma=min
+// console.log(`if loopas, jame suma=min ${suma}=${min}`);
+
+// } else {
+//     console.log(`Else loopas, jame suma=suma*i ${suma*i}=${suma}*${i}`);
+//     suma=suma*i;
+// }
+
+// }
+
+
+
+// 23. atspausdinti skaicius stringe nuo 44 iki 66 atskirtus "|| " zenklu
+
+
+
+
+
+// let sentence;
+// for (let i=44;i<=66;i++){
+// if (i===66) {
+//     sentence=`${sentence+i}`
+// }else if (i===44) {
+
+//     sentence=`${i}||`
+
+// }else{
+//     sentence=`${sentence+i}||`
+// }
+// }
+// console.log(sentence);
+
+
+
+// [09:55] Marius Kuzminskas
+// // 21. Sudeti skaicius nuo 17 iki 33. atspausdinti suma ir kiek buvo ciklu
+//  like 10
+
+// [09:56] Marius Kuzminskas
+// // 22. sudauginti skaicius nuo -4 iki 7 ir rezultata atspausdinti padalinta is 3.
+//  like 6
+
+
+
+
+
+
+// let cars = ["Saab", "Volvo", "BMW", "VW", "Audi"];
+// console.log(cars);
+
+// let x=10;
+// let asd=55;
+
+// let fake =asd;
+
+// console.log(asd);
+// console.log(fake);
+
+
+
+// for(let i=0;i=<5; i++){
+// let fake
+
+// }
+
+
+
+// // [09:55] Marius Kuzminskas
+// // // 21. Sudeti skaicius nuo 17 iki 33. atspausdinti suma ir kiek buvo ciklu
+// //  like 10
+
+// let nuo=17
+// let iki=33
+// let sum =0;
+// for(let i=nuo;i<=iki;i++){
+// sum +=i;
+// console.log(sum);
+// }
+
+
+
+
+
+
+
+
+
+// [09:56] Marius Kuzminskas
+// // 22. sudauginti skaicius nuo -4 iki 7 ir rezultata atspausdinti padalinta is 3.
+//  like 8
+
+// [09:57] Marius Kuzminskas
+// // 23. atspausdinti skaicius stringe nuo 44 iki 66 atskirtus "|| " zenklu
+//  like 11
+
+
+
+//  [12:06] Marius Kuzminskas
+// // 24. Suskaiciuoti kie yra skaiciau 3 kartotiniu nuo 12 iki 57
+//  like 9
+
+// [12:08] Marius Kuzminskas
+// // 25. suskaiciuoti koks butu vidurkis skaiciu nuo 45 iki 87
+//  like 8
+
+// [12:08] Marius Kuzminskas
+// // 26. sugeneruoti 3 random skaicius nuo 1 iki 10 ir pranesti:// a: ar buvo skaicius didesnis uz 7?// b: ar buvo skaicius mazesnis uz 3?
+//  like 5
+
